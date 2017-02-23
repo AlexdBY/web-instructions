ec8a76cf46ef09aaad9c1246c7065d2d26b8b2e6
AlexdBY <alexd.offmail@gmail.com>
2017-02-23 13:22:39 +0300

    Last commit fix

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,3 +1,19 @@
 /**
  * Created by Alex on 22.02.2017.
  */
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {TopuserstabComponent} from "./topuserstab/topuserstab.component";
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent,
+    children: [
+      {path: '', component: TopuserstabComponent}
+    ]
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
